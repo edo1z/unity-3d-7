3b4996addd2e239adc035c1796c8b8976d2bd965
web3ten0 <web3ten0@gmail.com>
2022-09-18 22:30:34 +0900

    他オブジェクトとの角度表示

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,12 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     private PlayerInput _input;
+    private GameObject _cube1;
 
     void Awake()
     {
         TryGetComponent(out _input);
+        _cube1 = GameObject.Find("Cube1");
     }
 
     private void OnEnable()
@@ -27,6 +29,12 @@ public class Player : MonoBehaviour
         transform.Rotate(0, 15f, 0);
         float deg = transform.localEulerAngles.y;
         Debug.Log("deg: " + deg);
+
+        Vector3 diff = _cube1.transform.position - transform.position;
+        float rad = Mathf.Atan2(diff.x, diff.z);
+        float deg2 = rad * Mathf.Rad2Deg;
+        Debug.Log("diff: " + diff);
+        Debug.Log("rad: " + rad + " deg2: " + deg2);
     }
 
 }
