7052c4bbd91a271af783a4492e1eacc5d0d266be
web3ten0 <web3ten0@gmail.com>
2022-09-19 00:15:48 +0900

    前後左右に移動

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,47 +6,61 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _smooth_time = 0.3f;
+    [SerializeField] private float _speed = 5f;
 
     private PlayerInput _input;
     private GameObject _cube1;
+    private CharacterController _characon;
+    private Vector3 _move_direction;
     private float _target_angle = 0f;
     private float _current_velocity = 0f;
 
     void Awake()
     {
         TryGetComponent(out _input);
+        TryGetComponent(out _characon);
         _cube1 = GameObject.Find("Cube1");
     }
 
     private void OnEnable()
     {
-        _input.actions["Rotate"].started += Rotate;
+        _input.actions["Move"].performed += OnMove;
+        _input.actions["Move"].canceled += OnMove;
+        _input.actions["Rotate"].started += OnRotate;
     }
 
     private void OnDisable()
     {
-        _input.actions["Rotate"].started -= Rotate;
+        _input.actions["Move"].performed -= OnMove;
+        _input.actions["Move"].canceled -= OnMove;
+        _input.actions["Rotate"].started -= OnRotate;
     }
 
-    private void Rotate(InputAction.CallbackContext obj)
+    private void OnMove(InputAction.CallbackContext obj)
+    {
+      Vector2 direction = obj.ReadValue<Vector2>();
+      _move_direction = new Vector3(direction.x, 0f, direction.y).normalized;
+    }
+
+    private void OnRotate(InputAction.CallbackContext obj)
     {
         _target_angle += 90f;
         if (_target_angle > 360) {
           _target_angle -= 360f;
         }
-
-        Vector3 diff = _cube1.transform.position - transform.position;
-        float rad = Mathf.Atan2(diff.x, diff.z);
-        float deg2 = rad * Mathf.Rad2Deg;
-        Debug.Log("diff: " + diff);
-        Debug.Log("rad: " + rad + " deg2: " + deg2);
     }
 
-    private void Update()
+    private void Move()
     {
         float current = transform.eulerAngles.y;
         float deg = Mathf.SmoothDampAngle(current, _target_angle, ref _current_velocity, _smooth_time);
         transform.rotation = Quaternion.Euler(0, deg, 0);
+        _characon.Move(_move_direction * _speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+      Move();
     }
 
 }
